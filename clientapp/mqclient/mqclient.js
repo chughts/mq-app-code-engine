f6933247a8021a83ed7a87ef8febd2202d4dfe62
Soheel Chughtai <soheel_chughtai@uk.ibm.com>
2021-11-26 14:39:09 +0000

    complete build cno logic

--- clientapp/mqclient/mqclient.js
+++ clientapp/mqclient/mqclient.js
@@ -1,5 +1,8 @@
 const mq = require('ibmmq');
 
+// Load up missing envrionment variables from the env.json file
+const env = require('../env.json');
+
 var MQC = mq.MQC;
 
 // Set up debug logging options
@@ -8,6 +11,30 @@ let debug_warn = require('debug')('mqapp-mqclient:warn');
 
 const _HCONNKEY = Symbol('hconn');
 const _HOBJKEY = Symbol('hObj');
+
+
+// Load the MQ Endpoint details either from the envrionment or from the
+// env.json file. The envrionment takes precedence.
+// The json file allows for
+// mulitple endpoints ala a cluster. The connection string is built
+// using the host(port) values for all the endpoints.
+// For all the other fields only the first
+// endpoint in the arryay is used.
+var MQDetails = {};
+
+['QMGR', 'QUEUE_NAME', 'HOST', 'PORT', 'MQ_PORT',
+ 'CHANNEL', 'KEY_REPOSITORY', 'CIPHER'].forEach(function(f) {
+  MQDetails[f] = process.env[f] || env.MQ_ENDPOINTS[0][f]
+});
+
+if (MQDetails['MQ_PORT']) {
+  MQDetails['PORT'] = MQDetails['MQ_PORT'];
+}
+
+var credentials = {
+  USER: process.env.APP_USER || env.MQ_ENDPOINTS[0].APP_USER,
+  PASSWORD: process.env.APP_PASSWORD || env.MQ_ENDPOINTS[0].APP_PASSWORD
+};
 
 
 class MQClient {
@@ -57,10 +84,10 @@ class MQClient {
     return new Promise((resolve, reject) => {
       this.buildCNO()
       .then((cno) => {
+        debug_info("CNO Built");
 
 
-
-        reject("Not yet ready - 001");
+        reject("Not yet ready - 002");
       })
       .catch((err) => {
         debug_warn("Error establising connection to MQ");
@@ -90,11 +117,63 @@ class MQClient {
       let cno = new mq.MQCNO();
       cno.Options = MQC.MQCNO_CLIENT_BINDING;
 
+      let csp = new mq.MQCSP();
+      csp.UserId = credentials.USER;
+      csp.Password = credentials.PASSWORD;
+      cno.SecurityParms = csp;
 
-      reject("CNO build logic not yet ready");
+      // And then fill in relevant fields for the MQCD
+      var cd = new mq.MQCD();
+
+      cd.ChannelName = MQDetails.CHANNEL;
+      cd.ConnectionName = this.getConnection();
+      debug_info('Connections string is ', cd.ConnectionName);
+
+      if (MQDetails.KEY_REPOSITORY) {
+        debug_info('Will be running in TLS Mode');
+
+        cd.SSLCipherSpec = MQDetails.CIPHER;
+        cd.SSLClientAuth = MQC.MQSCA_OPTIONAL;
+      }
+
+      // Make the MQCNO refer to the MQCD
+      cno.ClientConn = cd;
+
+      // The location of the KeyRepository is not specified in the CCDT, so regardless
+      // of whether a CCDT is being used, need to specify the KeyRepository location
+      // if it has been provided in the environment json settings.
+      if (MQDetails.KEY_REPOSITORY) {
+        debug_info('Key Repository has been specified');
+        // *** For TLS ***
+        var sco = new mq.MQSCO();
+
+        sco.KeyRepository = MQDetails.KEY_REPOSITORY;
+        // And make the CNO refer to the SSL Connection Options
+        cno.SSLConfig = sco;
+      }
+
+      resolve(cno);
     });
   }
 
+
+  getConnection() {
+    let points = [];
+
+    if (process.env['HOST'] && process.env['MQ_PORT']) {
+      let h = process.env['HOST'];
+      let p = process.env['MQ_PORT'];
+      points.push(`${h}(${p})`)
+    } else {
+      env.MQ_ENDPOINTS.forEach((p) => {
+        if (p['HOST'] && p['PORT']) {
+          points.push(`${p.HOST}(${p.PORT})`)
+        }
+      });
+    }
+
+    return points.join(',');
+  }
 
 
 }
